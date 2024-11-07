7c3197a2ad95aba7710e3f51c3ceb3d569caa882
choijiung0909 <chlwldnd1209@yonsei.ac.kr>
2024-11-07 21:59:15 +0900

    Title: 달에서 무게 구하기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241107/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double a = 0.165;
+        double b = 13;
+        double c = a*b;
+        System.out.printf("13 * 0.165000 = %.6f", c);
+    }
+}
